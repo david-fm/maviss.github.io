b7ae0aac0edf1aceabb07e36db03f5d01fc245f3
David Florez <dflorez02@gmail.com>
2021-02-12 16:47:55 +0100

    Cambio animaciones inicio

---
+++ scripts/efectoEscritura.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed("#spannnn", { 
+    strings:['Sea bienvenid@ al lugar donde los sueños se hacen realidad. ¿La buscas a ella? Lamento tener que decirte que se fue hace un rato gritando “¡Dígale que le dejé una pequeña carta que iniciará nuestro recorrido!” Esa pequeña energía nunca cambiará.^2500',
+        'Oh, tranquil@, ella está bien. Cuando llegó aquí nos comentó al personal y a mi que su siguiente destino era Nankurunaisa. Ella se veía bastante contenta mientras repetía “¡Es la hora, es la hora!”, tras eso nos dejó una carta para ti.^2500',
+        '¿A qué esperas? Necesitas ir a leerla para ver que te tiene preparado esa pelirroja.<a href= "./views/La-Carta.html" class="carta"><img src=./assets/media/img/_MG_4679.jpg class="carta-img"><a>^2500'], 
+    typeSpeed: 40
+}); 
