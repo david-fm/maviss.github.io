15cb9df452cd1d7166eb7ee9be7f5bf4e5c8c49b
David Florez Mazuera <crazymarketing.fm@gmail.com>
2021-04-01 16:26:39 +0200

    Final traspaso Django
    Se mantienen cosas por cambiar:
    ->Sobre Mi
    -> Sobresaliente abajo imagenes
    -> Next y prev listas
    -> Tienda

--- mavisDjango/portfolio/admin.py
+++ mavisDjango/portfolio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Portfolio
 
-# Register your models here.
+@admin.register(Portfolio)
+class EntradaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'img' )
--- mavisDjango/portfolio/models.py
+++ mavisDjango/portfolio/models.py
@@ -3,4 +3,3 @@ from django.db import models
 class Portfolio(models.Model):
     titulo = models.CharField('titulo', max_length=140)
     img = models.ImageField()
-    texto = models.TextField('texto', max_length=1000)
