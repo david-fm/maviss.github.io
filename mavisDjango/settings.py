109664cb2a3699a7da4b6b443a03aa5eaabe4ebf
David Florez Mazuera <crazymarketing.fm@gmail.com>
2021-04-04 21:22:55 +0200

    debug

--- mavisDjango/settings.py
+++ mavisDjango/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '(o8t^%c7jc2++ltv(740v(y+nevoqsajx1$k%&)*6y%f51pixj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['maviss-art.herokuapp.com','www.maviss.art', '.maviss.art']
 
