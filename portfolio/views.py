ec0207ce68f93d5f2f595343a3b116feee3891c0
David Florez Mazuera <crazymarketing.fm@gmail.com>
2021-04-05 14:56:40 +0200

    errors

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,12 +6,16 @@ class VistaPortfolio(TemplateView):
 
 class VistaDetalleUrbano(DetailView):
     model = Urbano
+    template_name = 'urbano_detail.html'
 
 class VistaUrbano(ListView):
     model = Urbano
+    template_name = 'urbano_list.html'
 
 class VistaDetallePaisajes(DetailView):
     model = Paisajes
+    template_name = 'paisajes_detail.html'
 
 class VistaPaisajes(ListView):
     model = Paisajes
+    template_name = 'paisajes_list.html'
